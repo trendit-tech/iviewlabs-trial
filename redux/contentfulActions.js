989a899dcc31dff1429e4941269159724030d9c5
sagar jhode <sagar.jhode@iviewlabs.net>
2023-05-01 21:15:30 +0530

    created details page

--- redux/contentfulActions.js
+++ redux/contentfulActions.js
@@ -1,21 +1,36 @@
-import * as contentful from 'contentful';
+import * as contentful from "contentful";
 
 const space = process.env.CONTENTFUL_SPACE;
 const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+const previewAccessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
 
-export const fetchContentfulData = () => {
+export const fetchContentfulData = (isPreview) => {
   return (dispatch) => {
-    const client = contentful.createClient({
-      space: space,
-      accessToken: accessToken
-    });
-    
-    client.getEntries()
-      .then(response => {
-        dispatch({ type: 'FETCH_CONTENTFUL_DATA_SUCCESS', payload: response.items });
+    let client;
+
+    if (isPreview) {
+      client = contentful.createClient({
+        space: space,
+        accessToken: previewAccessToken,
+        host: "preview.contentful.com",
+      });
+    } else {
+      client = contentful.createClient({
+        space: space,
+        accessToken: accessToken,
+      });
+    }
+
+    client
+      .getEntries()
+      .then((response) => {
+        dispatch({
+          type: "FETCH_CONTENTFUL_DATA_SUCCESS",
+          payload: response.items,
+        });
       })
-      .catch(error => {
-        dispatch({ type: 'FETCH_CONTENTFUL_DATA_FAILURE', error: error });
+      .catch((error) => {
+        dispatch({ type: "FETCH_CONTENTFUL_DATA_FAILURE", error: error });
       });
   };
 };
